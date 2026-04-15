e22f316e58bdcebeac3435c64cf1dd952371397c
szambon01 <113313497+szambon01@users.noreply.github.com>
2026-04-15 16:19:06 -0400

    Update projects.js

--- zambon-site/netlify/functions/projects.js
+++ zambon-site/netlify/functions/projects.js
@@ -1,23 +1,7 @@
-const fs = require('fs');
-const path = require('path');
-
 exports.handler = async () => {
-  try {
-    const filePath = path.join(__dirname, '../../projects.json');
-    const data = fs.readFileSync(filePath, 'utf8');
-    return {
-      statusCode: 200,
-      headers: { 
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*'
-      },
-      body: data,
-    };
-  } catch (err) {
-    return {
-      statusCode: 500,
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ error: err.message, path: __dirname })
-    };
-  }
+  return {
+    statusCode: 301,
+    headers: { Location: '/projects.json' },
+    body: ''
+  };
 };
